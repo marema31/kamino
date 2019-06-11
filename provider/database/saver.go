1576d6d6ff84631646738c8f44aeaaafa82016f7
Marc Carmier <mcarmier@gmail.com>
2019-06-11 23:42:04 +0200

    Add update database mode

--- provider/database/saver.go
+++ provider/database/saver.go
@@ -15,6 +15,7 @@ type dbSaverMode int
 const (
 	onlyIfEmpty dbSaverMode = iota // Will insert only if database was empty
 	insert      dbSaverMode = iota // Will insert all line from source (may break if primary key already present)
+	update      dbSaverMode = iota // Will update if line with same primary exist or skip the line
 	replace     dbSaverMode = iota // Will update if line with same primary exist or insert the line
 	exactCopy   dbSaverMode = iota // As replace but will remove line with primary key not present in source
 )
@@ -22,62 +23,120 @@ const (
 //DbSaver specifc state for database Saver provider
 type DbSaver struct {
 	kaminoDb
-	stmt     *sql.Stmt
-	colNames []string
-	mode     dbSaverMode
-	wasEmpty bool
+	insertStmt *sql.Stmt
+	updateStmt *sql.Stmt
+	colNames   []string
+	mode       dbSaverMode
+	wasEmpty   bool
+	key        string
+}
+
+func (ds *DbSaver) parseConfig(config map[string]string) error {
+	ds.key = config["key"]
+
+	ds.mode = exactCopy
+	modestr, ok := config["mode"]
+	if ok {
+		switch {
+		case strings.EqualFold(modestr, "onlyifempty"):
+			ds.mode = onlyIfEmpty
+		case strings.EqualFold(modestr, "insert"):
+			ds.mode = insert
+		case strings.EqualFold(modestr, "update"):
+			ds.mode = update
+		case strings.EqualFold(modestr, "replace"):
+			ds.mode = replace
+		case strings.EqualFold(modestr, "copy"):
+			ds.mode = exactCopy
+		}
+	}
+
+	if ds.key == "" && (ds.mode == update || ds.mode == replace) {
+		return fmt.Errorf("mode for %s.%s is %s and no key is provided", ds.database, ds.table, modestr)
+	}
+	return nil
 }
 
 //NewSaver open the database connection, prepare the insert statement and return a Saver compatible object
 func NewSaver(ctx context.Context, config map[string]string) (*DbSaver, error) {
+	var ds DbSaver
+	var err error
+
 	k, err := newKaminoDb(config)
 	if err != nil {
 		return nil, err
 	}
 
-	mode := exactCopy
-	modestr, ok := config["mode"]
-	if ok {
-		switch {
-		case strings.EqualFold(modestr, "onlyifempty"):
-			mode = onlyIfEmpty
-		case strings.EqualFold(modestr, "insert"):
-			mode = insert
-		case strings.EqualFold(modestr, "replace"):
-			mode = replace
-		case strings.EqualFold(modestr, "copy"):
-			mode = exactCopy
-		}
-	}
-	rows, err := k.db.QueryContext(ctx, fmt.Sprintf("SELECT * from %s LIMIT 1", k.table)) // We don't need data, we only needs the column names
+	ds.db = k.db
+	ds.driver = k.driver
+	ds.database = k.database
+	ds.table = k.table
+	ds.where = k.where
+
+	err = ds.parseConfig(config)
 	if err != nil {
 		return nil, err
+	}
+
+	err = ds.createStatement(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return &ds, nil
+}
+
+func (ds *DbSaver) createStatement(ctx context.Context) error {
+	rows, err := ds.db.QueryContext(ctx, fmt.Sprintf("SELECT * from %s LIMIT 1", ds.table)) // We don't need data, we only needs the column names
+	if err != nil {
+		return err
 	}
 
 	columns, err := rows.ColumnTypes()
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	columnsname := make([]string, len(columns))
-	questionmark := make([]string, len(columns))
-	for i, col := range columns {
-		questionmark[i] = "?"
-		columnsname[i] = col.Name()
+	var updateSet []string
+	var questionmark []string
+
+	for _, col := range columns {
+		if !strings.EqualFold(col.Name(), ds.key) {
+			questionmark = append(questionmark, "?")
+			ds.colNames = append(ds.colNames, col.Name())
+			updateSet = append(updateSet, fmt.Sprintf("%s=?", col.Name()))
+		}
 		//		fmt.Println(col.Name(), col.DatabaseTypeName(), col.ScanType())
 	}
+	// By doing like this we ensure the primary key will be the last of column names and this array can be use for insert and update
 
-	wasEmpty := !rows.Next()
-	if mode == onlyIfEmpty && !wasEmpty {
-		log.Printf("Warning: the table %s of database %s is not empty, I will do nothing on it", k.table, k.database)
+	if ds.key != "" {
+		questionmark = append(questionmark, "?")
+		ds.colNames = append(ds.colNames, ds.key)
 	}
 
-	stmt, err := k.db.Prepare(fmt.Sprintf("INSERT INTO %s ( %s) VALUES ( %s )", k.table, strings.Join(columnsname[:], ","), strings.Join(questionmark[:], ",")))
+	if len(columns) != len(ds.colNames) {
+		return fmt.Errorf("provided key %s is not a column of %s.%s ", ds.key, ds.database, ds.table)
+	}
+
+	ds.wasEmpty = !rows.Next()
+	if ds.mode == onlyIfEmpty && !ds.wasEmpty {
+		log.Printf("Warning: the table %s of database %s is not empty, I will do nothing on it", ds.table, ds.database)
+	}
+
+	ds.insertStmt, err = ds.db.Prepare(fmt.Sprintf("INSERT INTO %s ( %s) VALUES ( %s )", ds.table, strings.Join(ds.colNames[:], ","), strings.Join(questionmark[:], ",")))
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	return &DbSaver{*k, stmt, columnsname, mode, wasEmpty}, nil
+	if ds.mode == replace || ds.mode == update {
+		ds.updateStmt, err = ds.db.Prepare(fmt.Sprintf("UPDATE %s SET  %s WHERE %s = ?", ds.table, strings.Join(updateSet[:], ","), ds.key))
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 //Save writes the record to the destination
@@ -91,8 +150,18 @@ func (ds *DbSaver) Save(record common.Record) error {
 		row[i] = record[col]
 	}
 
-	_, err := ds.stmt.Exec(row...)
-	return err
+	switch ds.mode {
+	case onlyIfEmpty:
+		_, err := ds.insertStmt.Exec(row...)
+		return err
+	case insert:
+		_, err := ds.insertStmt.Exec(row...)
+		return err
+	case update:
+		_, err := ds.updateStmt.Exec(row...)
+		return err
+	}
+	return nil
 }
 
 //Close closes the destination
