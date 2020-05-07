a2d27e5d0f8a27cfe20f24ab274452a984be340a
Marc Carmier <mcarmier@gmail.com>
2020-05-07 17:37:03 +0200

    [FEATURE]If both admin and user migration wanted, do not fail if no admin migration available

--- step/migration/init.go
+++ step/migration/init.go
@@ -3,6 +3,7 @@ package migration
 import (
 	"context"
 	"os"
+	"path/filepath"
 
 	"github.com/Sirupsen/logrus"
 )
@@ -17,6 +18,22 @@ func (st *Step) Init(ctx context.Context, log *logrus.Entry) error {
 			log.Errorf("The migration folder %s does not exists", st.folder)
 			return err
 		}
+	}
+
+	if st.noAdmin {
+		return nil
+	}
+
+	adminFolder := filepath.Join(st.folder, "admin")
+	if _, err := os.Stat(adminFolder); err != nil {
+		if os.IsNotExist(err) && st.noUser {
+			log.Errorf("The admin migration folder %s does not exists and you asked me to apply it only", adminFolder)
+			return err
+		}
+
+		log.Infof("The admin migration folder %s does not exists ... skipping", adminFolder)
+
+		st.noAdmin = true
 	}
 
 	return nil
